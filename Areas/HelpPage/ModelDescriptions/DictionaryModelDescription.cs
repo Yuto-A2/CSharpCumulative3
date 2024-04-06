3b20d08bd24dd835fc015e17f55ae351b514e326
Yuto Arimori <n01648493@humber.ca>
2024-04-05 22:25:32 -0400

    upload

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SchoolDbContext.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Models/teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoolProject.Models 
+{
+    public class teacher
+    {
+        public int TeacherId { get; set; }
+        public string TeacherfName {  get; set; }
+        public string TeacherlName {  get; set; }
+        public string EmployeeNum { get; set; }
+        public DateTime HireDate { get; set; }  
+        public double Salary { get; set; }
+        public string className { get; set; }
+        public int classId { get; set; }
+    }
+}
